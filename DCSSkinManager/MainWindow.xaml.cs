ab8c110a1d4750a076aef34c0cc397b0867ca368
Racer396 <bhracer@yandex.ru>
2020-01-11 22:10:09 +0300

    App work now with MahApps.Metro theme Added aircraft icons Aircraft select buttons now autogenerate  in top ribbon

--- DCSSkinManager/MainWindow.xaml.cs
+++ DCSSkinManager/MainWindow.xaml.cs
@@ -1,38 +1,49 @@
-using System.Collections.Generic;
+using MahApps.Metro.Controls;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Net;
 using System.Windows;
-using System.Windows.Documents;
+using System.Windows.Controls;
 
 namespace DCSSkinManager
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
-        public ObservableCollection<UserFile> UserFiles { get; } = new ObservableCollection<UserFile>()
-        {
-            new UserFile(){Name = "TEST", Description = "TEST", Author = "THNDF", DownloadLink = "www.www.www", Date = "21.22.2222", Downloads = "0", Size = "5"}
-        };
+        public Holder FilesHolder = new Holder();
         public MainWindow()
         {
             InitializeComponent();
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 |
                                                    SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
-            DataContext = UserFiles;
         }
 
-        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        public class Holder
         {
-            if (e.AddedItems.Count == 1 && e.AddedItems[0] is UnitType craft)
+            public ObservableCollection<UserFile> UserFiles { get; } = new ObservableCollection<UserFile>()
+            {
+                new UserFile(){Name = "TEST", Description = "TEST", Author = "THNDF", DownloadLink = "www.www.www", Date = "21.22.2222", Downloads = "0", Size = "5"},
+                new UserFile(){Name = "TEST2", Description = "TEST2", Author = "AAA", DownloadLink = "www.www.www", Date = "21.22.2222", Downloads = "0", Size = "5"},
+                new UserFile(){Name = "TEST3", Description = "TEST3", Author = "CSDFSDF", DownloadLink = "www.www.www", Date = "21.22.2222", Downloads = "0", Size = "5"},
+                new UserFile(){Name = "TEST4", Description = "TEST4", Author = "BBCC", DownloadLink = "www.www.www", Date = "21.22.2222", Downloads = "0", Size = "5"},
+            };
+
+            public void Load(UserFiles files)
+            {
+                UserFiles.Clear();
+                files.Files.ForEach(UserFiles.Add);
+            }
+        }
+
+        private void OnModuleButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button button && button.DataContext is UnitType craft)
             {
                 var userFiles = new UserFiles(craft);
                 DataLoader.LoadUserFiles(userFiles);
-                UserFiles.Clear();
-                userFiles.Files.ForEach(x=>UserFiles.Add(x));
+                FilesHolder.Load(userFiles);
             }
         }
     }
---
+++ DCSSkinManager/Utils/EnumToIconsConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DCSSkinManager.Utils
+{
+    public class EnumToIconsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is UnitType unitType)
+            {
+                var image = new BitmapImage(new Uri($"Icons/{unitType:G}.png", UriKind.Relative));
+                return image;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string unitIconPath)
+            {
+                return Enum.GetValues(typeof(UnitType)).Cast<UnitType>().Single(
+                    x => string.Equals(unitIconPath.Replace("Icons\\", String.Empty).Replace(".img", String.Empty),
+                        x.ToString("G"))
+                );
+            }
+
+            return null;
+        }
+    }
+}
