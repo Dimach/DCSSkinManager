4f61da1c4ea81ba0de6ce3ebecc3e06a5398a478
Racer396 <bhracer@yandex.ru>
2020-01-12 19:16:09 +0300

    Options windows and icon pack

--- DCSSkinManager/Utils/EnumDescriptionConverter.cs
+++ DCSSkinManager/Utils/EnumDescriptionConverter.cs
@@ -32,25 +32,4 @@ namespace DCSSkinManager.Utils
             return base.ConvertTo(context, culture, value, destinationType);
         }
     }
-
-    public class EnumCollectionExtension : MarkupExtension
-    {
-        public Type EnumType { get; set; }
-
-        public override object ProvideValue(IServiceProvider _)
-        {
-            if (EnumType != null)
-            {
-                return CreateEnumValueList(EnumType);
-            }
-            return default(object);
-        }
-
-        private List<object> CreateEnumValueList(Type enumType)
-        {
-            return Enum.GetNames(enumType)
-                .Select(name => Enum.Parse(enumType, name))
-                .ToList();
-        }
-    }
 }
